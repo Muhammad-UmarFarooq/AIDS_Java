d7fdb9fafcbc9c7c32c9c61bd50e472a9513a8a0
Sadik Md <Sadikmd786@gmail.com>
2024-07-25 21:25:48 +0530

    hee

---
+++ JAVA/MatrixAdd_m1.java
@@ -0,0 +1,42 @@
+class MatrixAdd_m1
+{
+    public static void main(String args[])
+    {
+        int a[][]={{1,3,4},{2,4,3},{3,4,5}};
+        int b[][]={{1,3,4},{2,4,3},{1,2,4}};
+        // creating another matrix to store the sum of two matrix
+        int c[][]=new int[3][3];
+        // printing a matrix
+        System.out.println("A matrix is:");
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                System.out.print(a[i][j]+" ");
+            }
+            System.out.println();
+        }
+        // printing b matrix
+        System.out.println("B matrix is:");
+
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                System.out.print(b[i][j]+" ");
+            }
+            System.out.println();
+        }
+        // adding and printing addition of 2 matrices
+        System.out.println("Addition of A and B matrices is:");
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                c[i][j]=a[i][j]+b[i][j];
+                System.out.print(c[i][j]+" ");
+            }
+            System.out.println();
+        }               
+    }
+}
