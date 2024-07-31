6ec820e058e38fde393692369db4f5f0e1ac35e5
Muhammad-UmarFarooq <mdumar0508@gmail.com>
2024-07-31 09:55:42 +0530

    Update EvenOrOdd_m1.java

--- JAVA/EvenOrOdd_m1.java
+++ JAVA/EvenOrOdd_m1.java
@@ -3,6 +3,9 @@ public class evenorodd_m1
     public static void main(String args[])
     {
         int a[]={0,1,2,3,4,5,6,7,8,9};
+        System.out.println("The given array is:");
+        for(int i=0;i<a.length;i++)
+            
         System.out.println("Odd numbers:");
         for(int i=0;i<a.length;i++)
         {
