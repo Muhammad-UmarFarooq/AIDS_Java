5adecadc29b5eafe025922612c251d34cee6e9df
Muhammad-UmarFarooq <mdumar0508@gmail.com>
2024-07-31 10:38:27 +0530

    Update FibonacciSeries_m1.java

--- JAVA/FibonacciSeries_m1.java
+++ JAVA/FibonacciSeries_m1.java
@@ -4,7 +4,7 @@ class FibonacciSeries_m1
     public static void main(String args[])
     {
         int a=0,b=1,c,count=10;
-        // for printing 0 and 1
+        // For printing 0 and 1
         System.out.print(a+" "+b);
         for(int i=0;i<count;i++)
         {
