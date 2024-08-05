8d5822040a2fbf1debab649d553b20f0b393e473
Muhammad-UmarFarooq <mdumar0508@gmail.com>
2024-08-05 16:08:12 +0530

    Create AsciiValue_ex2_m1.java

---
+++ JAVA/AsciiValue_ex2_m1.java
@@ -0,0 +1,14 @@
+public class PrintAscii_ex2_m1
+{
+    public static void main(String args[])
+    {
+        // Characters whose ASCII value to be found
+        char c1='a';
+        char c2='b';
+        //Casting or converting a character into int type
+        int ascii1=(int)c1;
+        float ascii2=(float)c2;
+        System.out.println("The ASCII value of "+c1+" is: "+ascii1);
+        System.out.println("The ASCII value of "+c2+" is: "+ascii2);
+    }
+}
