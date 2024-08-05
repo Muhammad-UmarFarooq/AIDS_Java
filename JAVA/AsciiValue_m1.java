86e0da891130416dc6942997aafe57121967af8f
Muhammad-UmarFarooq <mdumar0508@gmail.com>
2024-08-05 16:02:13 +0530

    Create AsciiValue_m1.java

---
+++ JAVA/AsciiValue_m1.java
@@ -0,0 +1,14 @@
+public class PrintAscii_m1
+{
+    public static void main(String args[])
+    {
+        // Characters whose ASCII value to be found
+        char c1='a';
+        char c2='b';
+        // Variable that stores the integer value of the character
+        int asciivalue1=c1;
+        int asciivalue2=c2;
+        System.out.println("The ASCII value of "+c1+" is: "+asciivalue1);
+        System.out.println("The ASCII value of "+c2+" is: "+asciivalue2);
+    }
+}
